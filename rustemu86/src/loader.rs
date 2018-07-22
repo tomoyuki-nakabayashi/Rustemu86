616aed84d02883b681cd409609661cc03ff07335
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-22 13:19:56 +0900

    test: Verify the file contains a mov instruction.

--- rustemu86/src/loader.rs
+++ rustemu86/src/loader.rs
@@ -20,10 +20,21 @@ mod test {
   use super::*;
 
   #[test]
-  fn binary_load() {
-    let binary_reader = load("/home/tomoyuki/work/02.x86/Rustemu86/workspace/asms_for_test/mov");
-    assert!(binary_reader.is_ok());
+  fn load_mov_binary() {
+    let load_result = load("/home/tomoyuki/work/02.x86/Rustemu86/workspace/asms_for_test/mov");
+    assert!(load_result.is_ok());
 
+    let mut buffer = [0; 4];
+    let mut binary_file = load_result.unwrap();
+    binary_file.file.read(&mut buffer);
+
+    assert_eq!(0xb8, buffer[0]);
+    assert_eq!(0x00, buffer[1]);
+    assert_eq!(0x00, buffer[2]);
+    assert_eq!(0x00, buffer[3]);
+  }
+
+  fn load_failed() {
     let non_exist_file_open = load("./not_exist");
     assert!(non_exist_file_open.is_err());
   }
--- todo/todo.md
+++ todo/todo.md
@@ -2,9 +2,8 @@
 
 ## Load an x86 binary
 
-- [ ] Test to verify loaded file contains `mov ax, 0` e.g., `00b8 0000`
-- [x] Define BinaryReader struct.
-  - [x] Has FileHandler.
+- [x] Test to verify loaded file contains `mov ax, 0` e.g., `00b8 0000`
+  - [ ] Refactor binary assertion.
 - [ ] Open a binary given commandline option.
 
 ## CI
