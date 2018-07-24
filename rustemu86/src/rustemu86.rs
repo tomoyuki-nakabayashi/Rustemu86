3c0e30088cd40ff086b318d372a9a6ca1bbc1053
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-24 20:44:44 +0900

    feature: Mov immidiate to rcx.

--- rustemu86/src/rustemu86.rs
+++ rustemu86/src/rustemu86.rs
@@ -5,12 +5,20 @@ use loader::BinaryReader;
 pub struct Rustemu86 {
   // Must have cpu, memory, peripherals
   rax: u64,
+  rcx: u64,
 }
 
 impl Rustemu86 {
   fn execute_mov_imm_instruction(&mut self, inst: &[u8]) {
+    let dest = inst[0] & 0b00000111;
     let mut imm = &inst[2..];
-    self.rax = imm.read_u32::<LittleEndian>().unwrap().into();
+    let imm: u64 = imm.read_u32::<LittleEndian>().unwrap().into();
+
+    match dest {
+      0 => self.rax = imm,
+      1 => self.rcx = imm,
+      _ => ()
+    }
   }
 }
 
@@ -19,13 +27,18 @@ mod test {
   use super::*;
 
   #[test]
-  fn execute_an_instruction() {
+  fn execute_mov_imm_instruction() {
     let mut emu = Rustemu86{
       rax: 0xFFFFFFFF,
+      rcx: 0xFFFFFFFF,
     };
 
     let inst: &[u8] = &[0xb8, 0x00, 0x00, 0x00, 0x00, 0x00];
     emu.execute_mov_imm_instruction(&inst);
     assert_eq!(emu.rax, 0);
+
+    let inst: &[u8] = &[0xb9, 0x00, 0x00, 0x00, 0x00, 0x00];
+    emu.execute_mov_imm_instruction(&inst);
+    assert_eq!(emu.rcx, 0);
   }
 }
--- todo/todo.md
+++ todo/todo.md
@@ -5,15 +5,16 @@
 - [ ] Prepare Emulator structure to manage all of the emulation.
   - [x] Define structure.
 - [x] Prepare Register structure consists of rax.
+- [ ] Instruction decoder.
+- [ ] Instruction executor.
 
 ## Instruction
 
 - [x] mov immidiate instruction.
+- [ ] Prepare general registers.
+  - [x] rcx
+- [ ] mov register to register.
 
 ## Load an x86 binary
 
 - [ ] Load Elf format.
-
-## CI
-
-- [x] Setup Travis or Circle CI
