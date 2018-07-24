837e55159087a58f99e31e98db62cff5fe563efa
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-07-24 21:05:49 +0900

    feature: Mov immidiate rdx, rbx.

--- rustemu86/src/rustemu86.rs
+++ rustemu86/src/rustemu86.rs
@@ -6,10 +6,12 @@ pub struct Rustemu86 {
   // Must have cpu, memory, peripherals
   rax: u64,
   rcx: u64,
+  rdx: u64,
+  rbx: u64,
 }
 
 impl Rustemu86 {
-  fn execute_mov_imm_instruction(&mut self, inst: &[u8]) {
+  fn mov_imm(&mut self, inst: &[u8]) {
     let dest = inst[0] & 0b00000111;
     let mut imm = &inst[2..];
     let imm: u64 = imm.read_u32::<LittleEndian>().unwrap().into();
@@ -17,6 +19,8 @@ impl Rustemu86 {
     match dest {
       0 => self.rax = imm,
       1 => self.rcx = imm,
+      2 => self.rdx = imm,
+      3 => self.rbx = imm,
       _ => ()
     }
   }
@@ -27,18 +31,30 @@ mod test {
   use super::*;
 
   #[test]
-  fn execute_mov_imm_instruction() {
+  fn execute_mov_imm() {
     let mut emu = Rustemu86{
       rax: 0xFFFFFFFF,
       rcx: 0xFFFFFFFF,
+      rdx: 0xFFFFFFFF,
+      rbx: 0xFFFFFFFF,
     };
 
-    let inst: &[u8] = &[0xb8, 0x00, 0x00, 0x00, 0x00, 0x00];
-    emu.execute_mov_imm_instruction(&inst);
+    let mut insts: Vec<&[u8]> = Vec::with_capacity(4);
+    insts.push(&[0xb8, 0x00, 0x00, 0x00, 0x00, 0x00]);  // mov rax, 0
+    insts.push(&[0xb9, 0x00, 0x00, 0x00, 0x00, 0x00]);  // mov rcx, 0
+    insts.push(&[0xba, 0x00, 0x00, 0x00, 0x00, 0x00]);  // mov rdx, 0
+    insts.push(&[0xbb, 0x00, 0x00, 0x00, 0x00, 0x00]);  // mov rbx, 0
+
+    emu.mov_imm(&insts[0]);
     assert_eq!(emu.rax, 0);
 
-    let inst: &[u8] = &[0xb9, 0x00, 0x00, 0x00, 0x00, 0x00];
-    emu.execute_mov_imm_instruction(&inst);
+    emu.mov_imm(&insts[1]);
     assert_eq!(emu.rcx, 0);
+
+    emu.mov_imm(&insts[2]);
+    assert_eq!(emu.rdx, 0);
+
+    emu.mov_imm(&insts[3]);
+    assert_eq!(emu.rbx, 0);
   }
 }
--- todo/todo.md
+++ todo/todo.md
@@ -1,18 +1,21 @@
 # TO DO
 
+## Refactor
+
+- [ ] rustem86/test/execute_mov_imm can be expressed with loop of instructions.
+
 ## Emulator model
 
 - [ ] Prepare Emulator structure to manage all of the emulation.
   - [x] Define structure.
-- [x] Prepare Register structure consists of rax.
 - [ ] Instruction decoder.
 - [ ] Instruction executor.
+- [ ] CPU recieves an instrution.
 
 ## Instruction
 
 - [x] mov immidiate instruction.
 - [ ] Prepare general registers.
-  - [x] rcx
 - [ ] mov register to register.
 
 ## Load an x86 binary
