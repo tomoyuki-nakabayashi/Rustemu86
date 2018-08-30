56efc89c2bf4d3e8a05ca78a522dc634c44de7bd
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-08-30 21:29:36 +0900

    refactor: MemoryAccessTrait uses collect instead array initialization.

--- rustemu86/src/peripherals/memory_access.rs
+++ rustemu86/src/peripherals/memory_access.rs
@@ -1,5 +1,6 @@
 use byteorder::{LittleEndian, ReadBytesExt};
 
+#[derive(Debug)]
 pub struct MemoryAccessError;
 
 pub trait MemoryAccess {
@@ -18,12 +19,9 @@ pub trait MemoryAccess {
   }
 
   fn read_u64(&self, addr: usize) -> Result<u64, MemoryAccessError> {
-    let mut bytes: &[u8] = &[
-      self.read_u8(addr)?, self.read_u8(addr+1)?,
-      self.read_u8(addr+2)?, self.read_u8(addr+3)?,
-      self.read_u8(addr+4)?, self.read_u8(addr+5)?,
-      self.read_u8(addr+6)?, self.read_u8(addr+7)?];
-    //let v: Vec<u8> = (0..8).map(|x| self.read_u8(addr+x)? ).collect();
-    bytes.read_u64::<LittleEndian>().or(Err(MemoryAccessError{ }))
+    match (0..8).map(|x| self.read_u8(addr+x) ).collect::<Result<Vec<u8>, _>>() {
+      Ok(bytes) => (&bytes[..]).read_u64::<LittleEndian>().or(Err(MemoryAccessError{ })),
+      Err(err) => Err(err,)
+    }
   }
 }
