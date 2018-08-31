870fe05cb5e7ca41fd6ffc1532323dd73ff10916
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-08-31 13:13:53 +0900

    test: Add tests for read of MemoryAccessTrait.

--- rustemu86/src/peripherals/memory_access.rs
+++ rustemu86/src/peripherals/memory_access.rs
@@ -24,4 +24,34 @@ pub trait MemoryAccess {
       Err(err) => Err(err,)
     }
   }
+/* 
+  fn write_u8(&self, addr: usize, data: u8) -> Result<(), MemoryAccessError>;
+  fn write_u16(&self, addr: usize, data: u16) -> Result<(), MemoryAccessError>;
+  fn write_u32(&self, addr: usize, data: u32) -> Result<(), MemoryAccessError>;
+  fn write_u64(&self, addr: usize, data: u64) -> Result<(), MemoryAccessError>;
+ */
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  struct TestMemory(Vec<u8>);
+  impl MemoryAccess for TestMemory {
+    fn read_u8(&self, addr: usize) -> Result<u8, MemoryAccessError> {
+      Ok(self.0[addr])
+    }
+  }
+
+  #[test]
+  fn test_read() {
+    let buffer = vec![0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07];
+    let memory = TestMemory(buffer);
+    assert_eq!(memory.read_u8(0).unwrap(), 0x00);
+    assert_eq!(memory.read_u8(1).unwrap(), 0x01);
+
+    assert_eq!(memory.read_u16(0).unwrap(), 0x0100);
+    assert_eq!(memory.read_u32(0).unwrap(), 0x03020100);
+    assert_eq!(memory.read_u64(0).unwrap(), 0x0706050403020100);
+  }
 }
