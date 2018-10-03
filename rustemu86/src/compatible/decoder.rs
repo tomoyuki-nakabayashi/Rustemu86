c146d2b54953e37e89040cd6a1147272019d6bff
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-03 12:27:52 +0900

    refactor: Use default trait for ArithLogicInst.

--- rustemu86/src/compatible/decoder.rs
+++ rustemu86/src/compatible/decoder.rs
@@ -1,6 +1,7 @@
 use compatible::{Result, CompatibleException};
 use compatible::isa::opcode::OpcodeCompat;
 use compatible::fetcher::FetchedInst;
+use std::default::Default;
 
 pub(crate) enum ExecuteInst {
     ArithLogic(ArithLogicInst),
@@ -8,6 +9,8 @@ pub(crate) enum ExecuteInst {
 }
 
 pub(crate) struct ArithLogicInst {
+    left: u64,
+    right: u64,
     expr: Box<dyn Fn(u64, u64) -> u64>,
 }
 
@@ -17,16 +20,31 @@ impl ArithLogicInst {
     }
 }
 
+impl Default for ArithLogicInst {
+    fn default() -> ArithLogicInst {
+        ArithLogicInst {
+            left: 0,
+            right: 0,
+            expr: Box::new(nop),
+        }
+    }
+}
+
 pub(crate) struct PrivilegedInst {}
 
 pub(super) fn decode(inst: FetchedInst) -> Result<ExecuteInst> {
     match inst.get_opcode() {
         OpcodeCompat::Xor => {
-            Ok(ExecuteInst::ArithLogic( ArithLogicInst { expr: Box::new(|a, b| a ^ b ) }))
+            let inst = ArithLogicInst{expr: Box::new(|a, b| a ^ b ), ..Default::default()};
+            Ok(ExecuteInst::ArithLogic(inst))
         }
         OpcodeCompat::Hlt => {
             Ok(ExecuteInst::Privileged(PrivilegedInst{}))
         }
     }
 
+}
+
+fn nop(_left: u64, _right: u64) -> u64 {
+    0
 }
