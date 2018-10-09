5ed6f54344c7b307e3e54669e2dfa59adcb98775
tomoyuki-nakabayashi <tomo.wait.for.it.yuki@gmail.com>
2018-10-09 12:28:12 +0900

    refactor: Ad OpcodeFeature trait.

--- rustemu86/src/compatible/isa/opcode.rs
+++ rustemu86/src/compatible/isa/opcode.rs
@@ -10,3 +10,22 @@ use self::OpcodeCompat::*;
 pub fn inst_use_modrm(opcode: OpcodeCompat) -> bool {
     opcode == Xor
 }
+
+trait OpcodeFeature {
+    fn opcode(&self) -> OpcodeCompat;
+    fn use_modrm(&self) -> bool { false }
+}
+
+struct ArithRm {
+    opcode: OpcodeCompat,
+}
+
+impl OpcodeFeature for ArithRm {
+    fn opcode(&self) -> OpcodeCompat {
+        return self.opcode
+    }
+
+    fn use_modrm(&self) -> bool {
+        true
+    }
+}
